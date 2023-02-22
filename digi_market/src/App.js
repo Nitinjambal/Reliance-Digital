c21b2bd90be965174bf432f3c74c9ba9cd4325dd
nitin <nitinjam8800@gmail.com>
2023-02-23 02:22:32 +0530

    day-2

---
+++ digi_market/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import AllRoutes from './Components/AllRoutes';
+import ImageSlider from './Pages/ImageSlider';
+
+function App() {
+  return (
+    <div className="App">
+      <AllRoutes/>
+    <ImageSlider/>
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ digi_market/src/Pages/LoginPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import SubNavbar from '../Components/SubNavbar'
+import Navbar from '../Components/Navbar'
+
+function LoginPage() {
+  return (
+   <>
+   <Navbar/>
+   <SubNavbar/>
+   <div>I am a Login Page</div>
+   </>
+  )
+}
+
+export default LoginPage
